48681b4210db780829da92b67887df5cf724a994
Kartik-098-kushwaha <cu25250056@coeruniversity.ac.in>
2026-08-17 10:46:24 +0530

    Add files via upload

---
+++ labsheet2_kartik_CU25250056.py
@@ -0,0 +1,30 @@
+# def binary_search(arr, key):
+#     n = len(arr)
+#     for i in range(n):
+#         if arr[i] == key:
+#             return i
+#     return -1
+# arr = list(map(int, input("Enter the elements:").split()))
+# key = int(input("Enter the element to search: "))
+# result = binary_search(arr, key)
+# if result != -1:
+#     print("Element found at index:", result)
+# else:
+#     print("Element not found")
+
+# import queue
+
+# import numpy as np
+# arr = np.array([1, 2, 3])
+# print("Original Queue:", queue)
+
+# # Enqueue Operation
+# queue = np.append(arr, 4)
+# print("After Enqueue:", queue)
+
+# # Dequeue Operation
+# dequeued_element = queue[0]
+# print("Dequeued Element:", dequeued_element)
+
+# queue = np.delete(queue, 0)
+# print("After Dequeue:", queue)
